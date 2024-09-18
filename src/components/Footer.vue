f2061335ca3ea6357fea5b62dd2588e97a353128
Fl0wyn <feramusflorian@icloud.com>
2024-09-18 21:00:08 +0200

    remove wip and add github link

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,8 +6,11 @@ const props = defineProps({
 
 <template>
   <footer class="bg-white p-12 text-center font-thin text-slate-600">
-    Copyright ©
-    <span> {{ new Date().getFullYear() }} </span>
-    {{ title }}
+    Copyright © {{ new Date().getFullYear() }}
+    <a
+      href="https://github.com/Fl0wyn/rubikscube"
+      class="hover:text-slate-400"
+      >{{ title }}</a
+    >
   </footer>
 </template>
